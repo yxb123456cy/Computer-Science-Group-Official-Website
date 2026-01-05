f0640f5e62de7808c05557a245e9f721fcc6e161
yxb123456cy <2186256471@qq.com>
2026-01-05 11:12:52 +0800

    feat(component): :sparkles: 引入并全量使用ArcoDesignVue组件库

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
-import { createPinia } from 'pinia'
+import ArcoVue from '@arco-design/web-vue'
 
+// 引入ARcoDesignVue Icon图标库
+import ArcoVueIcon from '@arco-design/web-vue/es/icon'
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import router from './router'
 import './assets/main.css'
 
 const app = createApp(App)
-
+// 全量使用ArcoDesignVue
+app.use(ArcoVue)
+app.use(ArcoVueIcon)
 app.use(createPinia())
 app.use(router)
 
